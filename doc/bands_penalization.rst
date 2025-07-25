a357917e9ae9aeab84954e605f2ece27d1ccb5c7
Simone Innocente <simoneinnocente98@gmail.com>
2025-07-25 13:06:04 +0100

    Add files via upload

---
+++ doc/bands_penalization.rst
@@ -0,0 +1,16 @@
+Bands Penalization
+==================
+
+The `Bands penalization` module provides functionality to penalize irregularities in Raman spectra related to the bands.
+The module is divided into `Single Band` and `Band Region` functionalities, each designed to handle specific aspects of spectral data.
+More information can be found on the paper.
+
+Single Band
+-----------
+.. automodule:: IS_Score.bands_penalization.single_band
+   :members:
+
+Band Region
+-----------
+.. automodule:: IS_Score.bands_penalization.band_region
+   :members:
--- doc/conf.py
+++ doc/conf.py
@@ -5,24 +5,59 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
-release = '0.1'
+sys.path.insert(0, os.path.abspath('../..'))
 
+project = 'IS-Score'
+copyright = '2025, Simone Innocente'
+author = 'Simone Innocente'
+release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary'
+]
 
+html_theme = "pydata_sphinx_theme"
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
+
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+# Prevent Sphinx from adding the module name to the function names
+add_module_names = False
+
+html_title = 'IS-Score'
+html_sidebars = {
+  "**": []
+}
+html_context = {
+    "default_mode": "light"
+}
+
+html_theme_options = {
+    "navbar_end": ["navbar-icon-links","theme-switcher"],
+    "icon_links_label": "Quick Links",
+    "icon_links": [
+        # {
+        #     "name": "Slack",
+        #     "url": "https://deepchecks.com/slack",
+        #     "icon": "fab fa-github",
+        # },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/deepchecks/",
+            "icon": "fab fa-python",
+        }
+    ],
+}
